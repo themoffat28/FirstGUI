dd77fe9d0bfba321a45cb112d694dadfbac07de9
themoffat28 <bthemoffat@gmail.com>
2016-11-01 09:40:05 -0600

    Created Random color generator.

--- src/graphic/view/FirstPanel.java
+++ src/graphic/view/FirstPanel.java
@@ -2,6 +2,7 @@ package graphic.view;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.SpringLayout;
 import graphic.controller.FirstGUIController;
 import java.awt.Color;
 
@@ -13,43 +14,65 @@ public class FirstPanel extends JPanel
 	private FirstGUIController baseController;
 	private JButton colorButton;
 	private JButton randomButton;
-	
+	private SpringLayout baseLayout;
+
 	public FirstPanel(FirstGUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		
+
 		colorButton = new JButton("Color");
 		randomButton = new JButton("Random");
-		
+		baseLayout = new SpringLayout();
+
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
-	
+
 	private void setupPanel()
 	{
+		this.setLayout(baseLayout);
 		this.setBackground(Color.BLACK);
 		this.add(colorButton);
 		this.add(randomButton);
 	}
-	
+
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.NORTH, randomButton, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, randomButton, 10, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, colorButton, -10, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, colorButton, -10, SpringLayout.EAST, this);
+
 	}
-	
+
+	private void changeBackground()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		this.setBackground(new Color(red, green, blue));
+	}
+
 	private void setupListeners()
 	{
 		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
 			{
-				public void actionPerformed(ActionEvent click)
-				{
-					setBackground(Color.PINK);
-					System.out.println("Crap! Go back!");
-				}
-			});
+				setBackground(Color.PINK);
+				System.out.println("Crap! Go back!");
+			}
+		});
+
+		randomButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				changeBackground();
+			}
+		});
 	}
-			
-			
+
 }

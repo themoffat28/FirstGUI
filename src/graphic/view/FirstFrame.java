bbf23afd734eecd62b0b5c3dd0ed1d9516dcd1a1
themoffat28 <bthemoffat@gmail.com>
2016-11-01 09:11:24 -0600

    Created buttons and their actions.

--- src/graphic/view/FirstFrame.java
+++ src/graphic/view/FirstFrame.java
@@ -7,17 +7,21 @@ import java.awt.Dimension;
 public class FirstFrame extends JFrame
 {
 	private FirstGUIController baseController;
+	private FirstPanel appPanel;
 	
 	public FirstFrame(FirstGUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
+		appPanel = new FirstPanel(baseController);
 		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
+		this.setTitle("Hello, wuz good fam.");
 		this.setSize(new Dimension(500, 500));
 		this.setVisible(true);
 	}
--- src/graphic/view/FirstPanel.java
+++ src/graphic/view/FirstPanel.java
@@ -1,15 +1,55 @@
 package graphic.view;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import graphic.controller.FirstGUIController;
+import java.awt.Color;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FirstPanel extends JPanel
 {
 	private FirstGUIController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
 	
 	public FirstPanel(FirstGUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
+		
+		colorButton = new JButton("Color");
+		randomButton = new JButton("Random");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.BLACK);
+		this.add(colorButton);
+		this.add(randomButton);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		colorButton.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent click)
+				{
+					setBackground(Color.PINK);
+					System.out.println("Crap! Go back!");
+				}
+			});
+	}
+			
+			
 }
